2a6781b7c20a150639614823cf011c869f890817
Robson Fischer <fischerrobson@gmail.com>
2023-10-23 23:16:55 -0300

    spring basic setup

---
+++ src/main/java/br/com/fischer/ignitespring01/IgniteSpring01Application.java
@@ -0,0 +1,13 @@
+package br.com.fischer.ignitespring01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteSpring01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteSpring01Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fischer/ignitespring01/IgniteSpring01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fischer.ignitespring01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniteSpring01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
